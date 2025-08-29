9b1824ab2a0c3ef80c25269361ad83b4e9c682a1
Maruthi <maruthi19955@gmail.com>
2025-08-29 17:29:04 +0530

    vercel analytics added

--- src/main.jsx
+++ src/main.jsx
@@ -11,6 +11,7 @@ import "./index.css";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import "bootstrap/dist/css/bootstrap.min.css";
+import { Analytics } from "@vercel/analytics/react";
 
 import React from "react";
 import ReactDOM from "react-dom/client";
@@ -18,5 +19,6 @@ import App from "./App.jsx";
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <App />
+    <Analytics />
   </React.StrictMode>
 );
